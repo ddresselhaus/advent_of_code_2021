2752e726ae970bad275517e4f997acadd2bf9a40
Daniel Dresselhaus <ddresselhaus@gmail.com>
2021-12-01 19:01:06 -0500

    Day 01

---
+++ lib/solutions/day01.ex
@@ -0,0 +1,51 @@
+defmodule AdventOfCode2021.Solutions.Day01 do
+  def process_input(input) do
+    input
+    |> String.split("\n")
+    |> Enum.filter(fn x -> String.length(x) > 0 end)
+    |> Enum.map(&String.to_integer/1)
+  end
+
+  def execute(input) do
+    input
+    |> process_input()
+    |> total_increases()
+  end
+
+  def execute_2(input) do
+    {final, acc} =
+      input
+      |> process_input()
+      |> Enum.reduce({[], []}, fn x, {rolling, acc} ->
+        cond do
+          Enum.count(rolling) < 3 ->
+            {[x | rolling], acc}
+
+          true ->
+            [a, b, _c] = rolling
+            {[x, a, b], [Enum.sum(rolling) | acc]}
+        end
+      end)
+
+    Enum.reverse([Enum.sum(final) | acc])
+    |> total_increases()
+  end
+
+  def total_increases(input) do
+    {_, acc} =
+      Enum.reduce(input, {nil, []}, fn x, {previous, acc} ->
+        case previous do
+          nil ->
+            {x, acc}
+
+          _ ->
+            case x > previous do
+              true -> {x, [x | acc]}
+              false -> {x, acc}
+            end
+        end
+      end)
+
+    Enum.count(acc)
+  end
+end
---
+++ test/solutions/day01_test.exs
@@ -0,0 +1,26 @@
+defmodule AdventOfCode2021.Solutions.Day01Test do
+  use ExUnit.Case
+  alias AdventOfCode2021.Solutions.Day01
+
+  test "test 1" do
+    _test_input = """
+    199
+    200
+    208
+    210
+    200
+    207
+    240
+    269
+    260
+    263
+    """
+    input = File.read!("./lib/inputs/day01.txt")
+
+
+    result = Day01.execute(input)
+    result2 = Day01.execute_2(input)
+    assert result == 1226
+    assert result2 == 1252
+  end
+end
